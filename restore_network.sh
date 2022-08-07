871b6aae7b6171ce49ca7549c1c151045e2092dc
cr4sh <54245632+rkhunt3r@users.noreply.github.com>
2022-08-07 19:24:47 +0200

    Delete restore_network.sh

--- restore_network.sh
+++
@@ -1,3 +0,0 @@
-LC_ALL=C nmcli --fields UUID,TIMESTAMP-REAL con show | grep -v "UUID\|TIMESTAMP-REAL\|never" |  awk '{print $1}' | while read line;
-    do nmcli con up uuid $line &>/dev/null;
-    done
